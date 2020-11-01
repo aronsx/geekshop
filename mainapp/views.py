0a1645ae59f1926d1952818ff2cf846c32c04749
aronsx <aronsx@ya.ru>
2020-11-01 07:29:36 +0300

    add static

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,13 +1,25 @@
+import datetime
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'page_title': 'главная',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {
+        'page_title': 'каталог',
+    }
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    visit_date = datetime.datetime.now()
+    context = {
+        'page_title': 'контакты',
+        'visit_date': visit_date,
+    }
+    return render(request, 'mainapp/contact.html', context)
