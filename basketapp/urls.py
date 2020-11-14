d20276dcaae7a4736f49d8fced06add4543e2c58
aronsx <aronsx@ya.ru>
2020-11-14 07:25:02 +0300

    add basket with plug

---
+++ basketapp/urls.py
@@ -0,0 +1,12 @@
+import basketapp.views as basketapp
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+app_name = 'basketapp'
+
+urlpatterns = [
+    path('', basketapp.index, name='index'),
+]
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -23,6 +23,7 @@ from django.conf import settings
 urlpatterns = [
     path('', include('mainapp.urls', namespace='main')),
     path('auth/', include('authapp.urls', namespace='auth')),
+    path('basket/', include('basketapp.urls', namespace='basket')),
 
     path('admin/', admin.site.urls),
 ]
