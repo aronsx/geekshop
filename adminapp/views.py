72ca2b58b578a2995407009de096240850df9b4b
aronsx <aronsx@ya.ru>
2020-11-23 17:37:58 +0300

    update and delete user

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from adminapp.forms import AdminShopUserUpdateForm
 
 
 # Create your views here.
@@ -13,3 +17,36 @@ def index(request):
         'object_list': users
     }
     return render(request, 'adminapp/index.html', context=context)
+
+
+def user_delete(request, user_pk):
+    user = get_object_or_404(get_user_model(), pk=user_pk)
+    if not user.is_active or request.method == 'POST':
+        if user.is_active:
+            user.is_active = False
+            user.save()
+        return HttpResponseRedirect(reverse('myadmin:index'))
+    context = {
+        'page_title': 'админка/пользователи/удаление',
+        'object': user
+    }
+    return render(request, 'adminapp/user_delete.html', context=context)
+
+
+@user_passes_test(lambda x: x.is_superuser)
+def user_update(request, user_pk):
+    user = get_object_or_404(get_user_model(), pk=user_pk)
+    if request.method == 'POST':
+        user_form = AdminShopUserUpdateForm(request.POST, request.FILES, instance=user)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('myadmin:index'))
+    else:
+        user_form = AdminShopUserUpdateForm(instance=user)
+
+    context = {
+        'page_title': 'админка/пользователи/редактирование',
+        'form': user_form
+    }
+
+    return render(request, 'adminapp/user_update.html', context)
