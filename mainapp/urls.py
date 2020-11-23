7ba16b2b6cb4eb6438ad905c599af71f5076113c
aronsx <aronsx@ya.ru>
2020-11-24 01:47:22 +0300

    pagination

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -11,6 +11,9 @@ urlpatterns = [
     path('', mainapp.index, name='index'),
     path('catalog/', mainapp.products, name='products'),
     path('category/<int:category_pk>/', mainapp.category_items, name='category_items'),
+    path('category/<int:category_pk>/<int:page_num>/', mainapp.category_items, name='category_items_pages'),
+
+
     path('product/<int:product_pk>/', mainapp.product_page, name='product_page'),
     path('contact/', mainapp.contact, name='contact'),
 ]
