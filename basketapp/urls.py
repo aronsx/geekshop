96ec34d2841d3f7eeafe08dc292fb69da99e93fd
aronsx <aronsx@ya.ru>
2020-11-23 13:44:32 +0300

    AJAX on pure JS

--- basketapp/urls.py
+++ basketapp/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', basketapp.index, name='index'),
     path('add/<int:product_pk>', basketapp.add, name='add'),
     path('remove/<int:basket_pk>', basketapp.remove, name='remove'),
+    path('set/<int:basket_pk>/<int:qty>/', basketapp.set, name='set'),
 ]
--- static/js/basket_scripts.js
+++ static/js/basket_scripts.js
@@ -2,9 +2,9 @@
 
 window.onload = function () {
     console.log("DOM ready");
-    $('.product_price').on('click', function (event) {
-        console.log("target:", event.target)
-    })
+    // $('.product_price').on('click', function (event) {
+    //     console.log("target:", event.target)
+    // })
     // $('.basket_record').on('click', '.category_name',
     //     function (event) {
     //         console.log("target:", event.target);
@@ -17,7 +17,13 @@ window.onload = function () {
             let qty = event.target.value
             let pk = event.target.name
             console.log("qty:" + qty + ", pk " + pk);
-            // ajax
+            $.ajax({
+                url:"/basket/set/" + pk + "/" + qty + "/",
+                success: function (answer) {
+                    console.log("answer", answer);
+                    document.querySelector('.basket_list').innerHTML = answer.basket_list;
+                }
+            })
         }
     })
 }
