618a04b79cdd1ad95e62371d8734a273ad1d66c4
aronsx <aronsx@ya.ru>
2020-11-23 21:01:20 +0300

    cbv

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,9 +5,10 @@ from django.urls import path
 app_name = 'adminapp'
 
 urlpatterns = [
-    path('', adminapp.index, name='index'),
-    path('user/delete/<int:user_pk>/', adminapp.user_delete, name='user_delete'),
-    path('user/update/<int:user_pk>/', adminapp.user_update, name='user_update'),
+    path('', adminapp.ShopUserList.as_view(), name='index'),
+    path('user/create/', adminapp.ShopUserCreateView.as_view(), name='user_create'),
+    path('user/update/<int:user_pk>/', adminapp.ShopUserUpdateView.as_view(), name='user_update'),
+    path('user/delete/<int:user_pk>/', adminapp.ShopUserDeleteView.as_view(), name='user_delete'),
 
     path('categories/', adminapp.categories, name='categories'),
 ]
