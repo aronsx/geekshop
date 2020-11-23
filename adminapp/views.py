701e41ef3c594c0465c525b855bc1679bba2692f
aronsx <aronsx@ya.ru>
2020-11-23 21:15:38 +0300

    mixins

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,42 +1,53 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.urls import reverse, reverse_lazy
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from adminapp.forms import AdminShopUserUpdateForm, AdminShopUserCreateForm
 from mainapp.models import ProductsCategory
 
 
-class ShopUserList(ListView):
+class SuperUserOnlyMixin:
+    @method_decorator(user_passes_test(lambda x: x.is_superuser))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+
+class SetPageTitleMixin:
+    page_title = ''
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['page_title'] = self.page_title
+        return context
+
+
+class ShopUserList(SuperUserOnlyMixin, SetPageTitleMixin, ListView):
     model = get_user_model()
+    page_title = 'админка/пользователи'
 
 
-class ShopUserCreateView(CreateView):
+class ShopUserCreateView(SuperUserOnlyMixin, SetPageTitleMixin, CreateView):
     model = get_user_model()
     form_class = AdminShopUserCreateForm
     success_url = reverse_lazy('myadmin:index')
 
 
-class ShopUserUpdateView(UpdateView):
+class ShopUserUpdateView(SuperUserOnlyMixin, SetPageTitleMixin, UpdateView):
     model = get_user_model()
     form_class = AdminShopUserUpdateForm
     success_url = reverse_lazy('myadmin:index')
     pk_url_kwarg = 'user_pk'
+    page_title = 'админка/пользователи/редактирование'
 
 
-class ShopUserDeleteView(DeleteView):
+class ShopUserDeleteView(SuperUserOnlyMixin, SetPageTitleMixin, DeleteView):
     model = get_user_model()
     success_url = reverse_lazy('myadmin:index')
     pk_url_kwarg = 'user_pk'
-
-    def delete(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        success_url = self.get_success_url()
-        self.object.is_active = False
-        self.object.save()
-        return HttpResponseRedirect(success_url)
+    page_title = 'админка/пользователи/удаление'
 
 
 @user_passes_test(lambda x: x.is_superuser)
