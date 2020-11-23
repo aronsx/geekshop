3df092fc38b9e8ce96b8014ab14ef34ba727c00c
aronsx <aronsx@ya.ru>
2020-11-23 16:25:01 +0300

    jquery

--- static/js/basket_scripts.js
+++ static/js/basket_scripts.js
@@ -2,28 +2,28 @@
 
 window.onload = function () {
     console.log("DOM ready");
-    // $('.product_price').on('click', function (event) {
-    //     console.log("target:", event.target)
-    // })
-    // $('.basket_record').on('click', '.category_name',
-    //     function (event) {
-    //         console.log("target:", event.target);
-    //         // .val()
-    //     })
-    // let categoryNames = document.querySelectorAll('.category_name');
-    let categoryNames = document.querySelectorAll('input[type=number]');
-    categoryNames.forEach(function (item) {
-        item.onchange = function (event) {
-            let qty = event.target.value
-            let pk = event.target.name
-            console.log("qty:" + qty + ", pk " + pk);
-            $.ajax({
-                url:"/basket/set/" + pk + "/" + qty + "/",
-                success: function (answer) {
-                    console.log("answer", answer);
-                    document.querySelector('.basket_list').innerHTML = answer.basket_list;
-                }
-            })
-        }
+    $('.basket_list').on('change', 'input[type=number]', function (event) {
+        $.ajax({
+            url: "/basket/set/" + event.target.name + "/" + event.target.value + "/",
+            success: function (answer) {
+                document.querySelector('.basket_list').innerHTML = answer.basket_list;
+            }
+        });
     })
+    // let categoryNames = document.querySelectorAll('input[type=number]');
+    // categoryNames.forEach(function (item) {
+    //     item.onchange = function (event) {
+    //         let qty = event.target.value
+    //         let pk = event.target.name
+    //         console.log("qty:" + qty + ", pk " + pk);
+    //         $.ajax({
+    //             url: "/basket/set/" + pk + "/" + qty + "/",
+    //             success: function (answer) {
+    //                 console.log("answer", answer);
+    //                 document.querySelector('.basket_list').innerHTML = answer.basket_list;
+    //             }
+    //         })
+    //     }
+    // })
 }
+
